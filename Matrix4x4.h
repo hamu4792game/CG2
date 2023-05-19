3bd375dfa13c9861930a7cc4ebe660227746b922
hamu4792game <112112847+hamu4792game@users.noreply.github.com>
2023-05-19 16:23:33 +0900

    Make関数をクラスから削除

--- Matrix4x4.h
+++ Matrix4x4.h
@@ -24,36 +24,35 @@ public:
 	//	行列の代入
 	Matrix4x4 operator=(const Matrix4x4& mat);
 	
-public:
-	//	逆行列
-	friend Matrix4x4 Inverse(const Matrix4x4& m);
-	//	転置行列
-	friend Matrix4x4 Transpose(const Matrix4x4& m);
-	//	単位行列の作成
-	friend Matrix4x4 MakeIdentity4x4();
-	//	平行移動行列
-	friend Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
-	//	拡大縮小行列
-	friend Matrix4x4 MakeScaleMatrix(const Vector3& scale);
-	//	座標変換
-	friend Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
-	//	X軸回転行列
-	friend Matrix4x4 MakeRotateXMatrix(float radian);
-	//	Y軸回転行列
-	friend Matrix4x4 MakeRotateYMatrix(float radian);
-	//	Z軸回転行列
-	friend Matrix4x4 MakeRotateZMatrix(float radian);
-	//	3次元アフィン変換行列
-	friend Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
-
-	//	透視投影行列
-	friend Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
-	//	正射影行列
-	friend Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
-	//	ビューポート変換行列
-	friend Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
-
-	//	クロス積
-	friend Vector3 Cross(const Vector3& v1, const Vector3& v2);
-
 };
+
+//	逆行列
+Matrix4x4 Inverse(const Matrix4x4& m);
+//	転置行列
+Matrix4x4 Transpose(const Matrix4x4& m);
+//	単位行列の作成
+Matrix4x4 MakeIdentity4x4();
+//	平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+//	拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+//	座標変換
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+//	X軸回転行列
+Matrix4x4 MakeRotateXMatrix(float radian);
+//	Y軸回転行列
+Matrix4x4 MakeRotateYMatrix(float radian);
+//	Z軸回転行列
+Matrix4x4 MakeRotateZMatrix(float radian);
+//	3次元アフィン変換行列
+Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+//	透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+//	正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+//	ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+
+//	クロス積
+Vector3 Cross(const Vector3& v1, const Vector3& v2);
